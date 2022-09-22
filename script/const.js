fa8b22fd96bc02187e7d895bbc9905f9625e2e71
Mirkek <asen.mirkhat@bk.ru>
2022-09-22 15:31:41 +0600

    zoom regions

--- script/const.js
+++ script/const.js
@@ -1,11 +1,24 @@
 let WORLD_SVG_ID = "worldSvg";
 let KZ_SVG_ID = "kzSvgMap";
 let KZ_SVG_TITLE_ID = "kzSvgMapTitle";
-let KZ_SVG_ICON_CLASS = "kzMapIcon";
+let ALL_SVG_ICON_CLASS = "AllMapIcon";
+let KZ_DETAIL_CLASS = "kzDetailClass";
+let ALL_MAP_TITLE_CLASS = "AllMapTitle";
+let KZ_SVG_DETAILS = "kzDetailsSvg";
+let KZ_ITEM = "kzItem";
 
 let worldSvg = document.getElementById(WORLD_SVG_ID);
 let kzSvgMap = document.getElementById(KZ_SVG_ID);
 let kzSvgTitle = document.getElementById(KZ_SVG_TITLE_ID);
-let kzMapIcon = document.getElementsByClassName(KZ_SVG_ICON_CLASS);
+let allIcons = document.getElementsByClassName(ALL_SVG_ICON_CLASS);
+let allTitles = document.getElementsByClassName(ALL_MAP_TITLE_CLASS);
+let kzSvgDetails = document.getElementById(KZ_SVG_DETAILS);
+let kzSvgItem = document.querySelector("#kzDetailsSvg");
+let allKzSvgItems = kzSvgItem.children;
 
-var elements = document.getElementsByClassName(KZ_SVG_ICON_CLASS);
+
+ATYRAU_ICON_CLASS = "atyrauIcon";
+ATYRAU_REGION_ID = "atyrauRegion";
+
+let atyrauRegionSvg = document.getElementById(ATYRAU_REGION_ID);
+let atyrauIconSvg = document.getElementsByClassName(ATYRAU_ICON_CLASS);
--- script/script.js
+++ script/script.js
@@ -2,22 +2,54 @@ document.addEventListener('click', (e) => {
     let elementId = e.target.id;
     let classNameOfSvg = e.target.className.baseVal;
 
-    if (elementId === KZ_SVG_ID || elementId === KZ_SVG_TITLE_ID || classNameOfSvg === KZ_SVG_ICON_CLASS) {
+
+    if (elementId === KZ_SVG_ID ||
+        elementId === KZ_SVG_TITLE_ID ||
+        classNameOfSvg === ALL_SVG_ICON_CLASS ||
+        classNameOfSvg === KZ_ITEM ||
+        classNameOfSvg === KZ_DETAIL_CLASS) {
         worldSvg.classList.add("zoom");
         kzSvgMap.classList.add("hide");
         kzSvgTitle.classList.add("hide");
+        kzSvgDetails.classList.add("zoom");
 
-        for (var i = 0; i < elements.length; i++) {
-            elements[i].classList.add("hide");
+        for (var i = 0; i < allIcons.length; i++) {
+            allIcons[i].classList.add("hide");
         }
-    } else {
+        for (var i = 0; i < allTitles.length; i++) {
+            allTitles[i].classList.add("hide");
+        }
+        for (var i=0, max = allKzSvgItems.length; i < max; i++) {
+            allKzSvgItems[i].classList.add("kzItem");
+        }
+        console.log("zoom to kz");
+    }
+    else if (classNameOfSvg === ATYRAU_ICON_CLASS || elementId === ATYRAU_REGION_ID) {
+        worldSvg.classList.add("zoomToAtyrau")
+        console.log("zoom to atyrau");
+    }
+    else {
+        worldSvg.classList.remove("zoomToAtyrau");
         worldSvg.classList.remove("zoom");
         kzSvgMap.classList.remove("hide");
         kzSvgTitle.classList.remove("hide");
+        kzSvgDetails.classList.remove("zoom");
 
-        for (var i = 0; i < elements.length; i++) {
-            elements[i].classList.remove("hide");
+        for (var i = 0; i < allIcons.length; i++) {
+            allIcons[i].classList.remove("hide");
         }
+        for (var i = 0; i < allTitles.length; i++) {
+            allTitles[i].classList.remove("hide");
+        }
+        for (var i=0, max = allKzSvgItems.length; i < max; i++) {
+            allKzSvgItems[i].classList.remove("kzItem");
+        }
+        console.log("zoom out");
     }
+    // console.log(elementId);
+    // console.log(classNameOfSvg);
 });
+
+
+
 
